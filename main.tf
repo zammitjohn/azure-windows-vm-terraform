3409e8e6743b0a27e48755dccd2c820b6946ad91
John Zammit <john.zammit@alludo.com>
2023-06-23 21:17:00 +0200

    Include timestamp in resource group name

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "demo" {
-  name     = "${var.prefix}-rg"
-  location = var.location # this is where RG metadata is saved. Resources can be assigned to different region.
+  name     = "${var.prefix}-rg-${join("", split(":", timestamp()))}" # removing the colons since Azure doesn't allow them.
+  location = var.location                                            # this is where RG metadata is saved. Resources can be assigned to different region.
 }
